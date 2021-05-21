edea09b3992646b5588730344cc4e9a5515d049c
Manjunath D <56871357+Manjuphoenix@users.noreply.github.com>
2021-05-21 09:48:10 +0530

    Not solved (Day -2)

--- duplicateZeros/src/duplicateZeros.java
+++ duplicateZeros/src/duplicateZeros.java
@@ -2,27 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] duplicateZeros(int[] nums) {
-        for (int i = 0; i < nums.length; i++){
-            if ((nums[i] == 0)){
-                nums[i+1] = 0;
-            }
+        for (int i = 0; i < nums.length - 1; i++) {
+           if (nums[i] == 0){
+               nums[i+1] = 0;
+               nums[i] = nums[i+1];
+           }
         }
         return nums;
     }
 }
 
 class main {
-    public static void main(String[]  args){
+    public static void main(String[] args) {
         Solution s = new Solution();
         Scanner in = new Scanner(System.in);
         int number = in.nextInt();
         int[] nums = new int[number];
-        for (int i = 0; i < number; i++){
+        for (int i = 0; i < number; i++) {
             nums[i] = in.nextInt();
         }
         int[] arr = s.duplicateZeros(nums);
-        for (int i = 0; i < number; i++){
-            System.out.print(arr[i]+ "\t");
+        for (int i = 0; i < number; i++) {
+            System.out.print(arr[i] + "\t");
         }
     }
 }
