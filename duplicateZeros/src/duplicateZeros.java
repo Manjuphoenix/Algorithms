032dc9383f4ad70f891fad998dc2250bdb996de4
Manjunath D <56871357+Manjuphoenix@users.noreply.github.com>
2021-05-21 09:47:25 +0530

    Add files via upload
    Not solved (day -1)

---
+++ duplicateZeros/src/duplicateZeros.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] duplicateZeros(int[] nums) {
+        for (int i = 0; i < nums.length; i++){
+            if ((nums[i] == 0)){
+                nums[i+1] = 0;
+            }
+        }
+        return nums;
+    }
+}
+
+class main {
+    public static void main(String[]  args){
+        Solution s = new Solution();
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        int[] nums = new int[number];
+        for (int i = 0; i < number; i++){
+            nums[i] = in.nextInt();
+        }
+        int[] arr = s.duplicateZeros(nums);
+        for (int i = 0; i < number; i++){
+            System.out.print(arr[i]+ "\t");
+        }
+    }
+}
